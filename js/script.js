9fcf6bea57cedac7e56ae1b366ff18d72de2705f
desireemezzotero <desireemezzotero1@gmail.com>
2024-12-03 16:51:33 +0100

    [add] li

--- js/script.js
+++ js/script.js
@@ -2,12 +2,15 @@ const endopoint = "https://flynn.boolean.careers/exercises/api/random/mail";
 
 let email = [ ]
 
+const ul = document.querySelector('ul')
+
 for(let i= 0; i < 10; i++) {
   axios.get(endopoint)
   .then (response => { 
     email.push(response.data.response);
     const li = document.createElement('li');
-    li.append = `${email}`
+    li.append(response.data.response);
+    ul.append(li)
   console.log(email)
   })
 
