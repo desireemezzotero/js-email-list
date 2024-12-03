526c40b95503e2d205e18a388b8e479c38c68409
desireemezzotero <desireemezzotero1@gmail.com>
2024-12-03 16:36:48 +0100

    correct differents emails

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 const endopoint = "https://flynn.boolean.careers/exercises/api/random/mail"; 
 
-let email;
-axios.get(endopoint)
-.then (response => { 
-  for (let i = 0; i < 10; i++) {
-    email += response.data.response;
+let email = [ ]
+
+for(let i= 0; i < 10; i++) {
+  axios.get(endopoint)
+  .then (response => { 
+    email.push(response.data.response);
     const li = document.createElement('li');
-    li.append = `${email} `
-  }
+    li.append = `${email}`
   console.log(email)
-})
-.catch (error => {
- console.log(error)
-}) 
- 
+  })
+
+  .catch (error => {
+    console.log(error)
+   })
+}
